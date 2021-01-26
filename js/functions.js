7473bbc000e25c93db45a72a77628913ab060d2a
fernando <fernom95@gmail.com>
2021-01-26 10:08:28 -0600

    nubes moviendose

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,19 @@
 //Aqui van las funciones
-function updateCanvas(){
-    ImagenInit.mover()
-    ctx.clearRect(0, 0, 900, 600)
-    ImagenInit.dibujar()
-    requestAnimationFrame(updateCanvas)
-}
+
 
 
 function revisarNivel(){
+    nivel.innerText = `Nivel: ${numNivel}`
+}
+
+
+function updateCanvas(){
+    revisarNivel()
+    nubes.mover() //no se pone el nombre de la clase, se pone el nuevo objeto que creaste porque la clase es solo  "la fabrica" aún no le has pasado nada
+    ctx.clearRect(0, 0, 900, 600)
+    fondo.dibujar()
+    nubes.dibujar()
+    requestAnimationFrame(updateCanvas)
 
 }
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // alert("funciona")
-const canva = document.querySelector("#canva")
-ctx = canva.getContext('2d')
 
-let frames = 0
-let vidas = 3
-let nivel = 0
+
+
+
+
+
+
+
